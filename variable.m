e8d030dca8a0749301e3c2f26dee8245d6ef53ba
Sarah Geilenkrichen <r0897953@luan.kuleuven.be>
2026-04-28 12:04:02 +0200

    Working basemodel in number 3

--- variable.m
+++ variable.m
@@ -27,22 +27,25 @@ if default == 1
     transfo_Rm = 500;
     transfo_Lm = Inf;
 elseif default == 0
-    %submarine
+    % Onze submarine waarden submarine
     sub_posR = 0.036;
-    sub_zeroR = 0.108;
+    sub_zeroR = 0.09;
     
-    sub_posR = sub_posR/16;
-    sub_zeroR = sub_zeroR/16;
+    sub_posR = sub_posR/2;
+    sub_zeroR = sub_zeroR/2;
 
     sub_posInd = 0.0014;
     sub_zeroInd = 0.0035;
+    
+    sub_posInd = sub_posInd/2;
+    sub_zeroInd = sub_zeroInd/2;
 
     sub_posCap = 0.16e-006;
-    sub_zeroCap = 0.053333e-006;
+    sub_zeroCap = 0.064e-006;
     
-    sub_posCap = sub_posCap*2.5;
-    sub_zeroCap = sub_zeroCap*2.5;
-    
+    sub_posCap = sub_posCap*2;
+    sub_zeroCap = sub_zeroCap*2;
+
     
 %     sub_posCap = 0.005e-006;
 %     sub_zeroCap = 0.0016e-006;
@@ -54,15 +57,27 @@ elseif default == 0
     
 %     ohl_posCap = 0.01e-006/2; 
 %     ohl_zeroCap = 0.003333e-006/2;
-
-    ohl_posR = 0.1153/4;
-    ohl_zeroR = 0.4138/4;
+%ONZE OHL WAARDEN
+%     ohl_posR = 0.1153/16;
+%     ohl_zeroR = 0.4138/16;
+%     
+%     ohl_posInd = (1.05e-3)/16;
+%     ohl_zeroInd = (3.32e-3)/16;
+%     
+%     ohl_posCap = (11.33e-009)*16; 
+%     ohl_zeroCap = (5e-009)*16;
     
-    ohl_posInd = (1.05e-3)/4;
-    ohl_zeroInd = (3.32e-3)/4;
+    ohl_posR = 0.0702/12;
+    ohl_zeroR = 0.2184/12;
     
-    ohl_posCap = (11.33e-009)*4; 
-    ohl_zeroCap = (5e-009)*4;
+    ohl_posInd = (1.2742e-3)/12;
+    ohl_zeroInd = (2.2385e-3)/12;
+    
+    ohl_posCap = (0.00903e-009)*12; 
+    ohl_zeroCap = (0.00542e-009)*12;
+    
+    
+    
     
 %     ohl_posR = ohl_posR*5;
 %     ohl_zeroR = ohl_zeroR*5;
@@ -85,10 +100,11 @@ elseif default == 0
 %     
     compensate_power = 2*pi*Vac^2*57*sub_posCap*50;
 %     sub_Ind = 40e6;
-    sub_Ind = compensate_power/4;
+    sub_Ind = compensate_power/2;
     sub_Cap = 0;
     
     ohl_Ind = 0;
+%     ohl_Cap = 2*pi*Vac^2*57*ohl_posInd*50;
     ohl_Cap = 75e6;
     
     transfo_R1 = 0.000866667;
